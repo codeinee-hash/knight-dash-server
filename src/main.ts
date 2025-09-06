f5907493610dad6c3ddb362f434ddd068e990394
eldyarcodes <thedolbekov2@gmail.com>
2025-09-06 22:14:49 +0600

    refactor

--- src/main.ts
+++ src/main.ts
@@ -15,17 +15,6 @@ async function run() {
 		allowedHeaders: ['Content-Type', 'Authorization'],
 	})
 
-	app.use((req, res, next) => {
-		res.header('Access-Control-Allow-Origin', 'http://localhost:5173')
-		res.header('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE,OPTIONS')
-		res.header('Access-Control-Allow-Headers', 'Content-Type,Authorization')
-		res.header('Access-Control-Allow-Credentials', 'true')
-		if (req.method === 'OPTIONS') {
-			return res.status(200).end()
-		}
-		next()
-	})
-
 	app.use(cookieParser())
 	app.useGlobalPipes(new ValidationPipe())
 
